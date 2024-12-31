649321fcc2161de6bc0cf7c0d6a8237afa4d912e
Oguz Turkay <tyserract@gmail.com>
2024-12-31 18:03:36 +0300

    Refactor route configuration to streamline route definitions for Videographer and Social Media Manager pages by removing layout wrapper. This change enhances code organization and simplifies route management.

--- app/routes.ts
+++ app/routes.ts
@@ -7,11 +7,6 @@ import {
 
 export default [
   index("routes/home.tsx"),
-  layout("routes/layout.tsx", [
-    route("videographer", "routes/positions/videographer/page.tsx"),
-    route(
-      "social-media-manager",
-      "routes/positions/social-media-manager/page.tsx"
-    ),
-  ]),
+  route("videographer", "routes/positions/videographer/page.tsx"),
+  route("social-media-manager", "routes/positions/social-media-manager/page.tsx"),
 ] satisfies RouteConfig;
