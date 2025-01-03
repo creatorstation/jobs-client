d4777c794ab89190e86d0461b610930e720bf97f
Oguz Turkay <tyserract@gmail.com>
2025-01-03 14:00:16 +0300

    refactor: consolidate Google and LinkedIn authentication in AuthButtons component
    - Replaced individual GoogleLogin and LinkedInLogin components with a unified AuthButtons component.
    - Implemented Google login using useGoogleLogin hook with success and error handling.
    - Removed the LinkedInLogin component and integrated LinkedIn authentication directly within AuthButtons.
    - Enhanced user experience with toast notifications for sign-in status and improved button styling.

--- app/components/AuthButtons.tsx
+++ app/components/AuthButtons.tsx
@@ -1,42 +1,84 @@
-import { GoogleLogin } from "@react-oauth/google";
-import { jwtDecode } from "jwt-decode";
+import { useGoogleLogin } from "@react-oauth/google";
 import { toast } from "react-toastify";
-import LinkedInLogin from "./LinkedinButton";
 import { userStore } from "~/store/user-store";
+import axios from "axios";
 
 interface AuthButtonsProps {
   redirPath: string;
 }
 
 export function AuthButtons({ redirPath }: AuthButtonsProps) {
+  const redirectToLinkedIn = () => {
+    const clientId = "77bdm9yb1yeig5";
+    const redirectUri = encodeURIComponent(
+      `${window.location.origin}/${redirPath}`
+    );
+    const state = Math.random().toString(36).substring(2);
+    const scope = "email openid profile ";
+
+    const linkedInAuthUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&scope=${scope}`;
+
+    window.location.href = linkedInAuthUrl;
+  };
+
+  const login = useGoogleLogin({
+    onSuccess: (tokenResponse) => {
+      axios
+        .get("https://www.googleapis.com/oauth2/v3/userinfo", {
+          params: {
+            access_token: tokenResponse.access_token,
+          },
+        })
+        .then((response) => {
+          updateUserData({
+            email: response.data.email,
+            name: response.data.name,
+          });
+          toast("Signed in successfully!", { type: "success" });
+        });
+    },
+    onError: (error) => {
+      console.log(error);
+      toast("An error occurred while signing in.", { type: "error" });
+    },
+    flow: "implicit",
+  });
+
   const { updateUserData } = userStore();
 
   return (
     <>
       <div className="mt-4 flex justify-center">
         <div className="w-64">
-          <GoogleLogin
-            size="large"
-            onSuccess={(credentialResponse) => {
-              console.log(credentialResponse);
-              const decoded = jwtDecode<any>(
-                credentialResponse.credential as string
-              );
-              updateUserData(decoded);
-              toast("Signed in successfully!", { type: "success" });
-            }}
-            onError={() => {
-              toast("An error occurred while signing in.", { type: "error" });
-            }}
-            useOneTap={true}
-            text="continue_with"
-          />
+          <button
+            onClick={() => login()}
+            className="w-64 px-2 py-2 border flex gap-2 border-slate-200 rounded text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
+          >
+            <img
+              className="w-6 h-6"
+              src="https://www.svgrepo.com/show/452216/google.svg"
+              loading="lazy"
+              alt="linkedin logo"
+            />
+            <span className="pt-0.5 text-sm pl-5">Continue with Google</span>
+          </button>
         </div>
       </div>
 
       <div className="mt-4 flex justify-center">
         <div className="w-64">
-          <LinkedInLogin redirPath={redirPath} />
+          <button
+            onClick={redirectToLinkedIn}
+            className="w-64 px-2 py-2 border flex gap-2 border-slate-200 rounded text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
+          >
+            <img
+              className="w-6 h-6"
+              src="https://www.svgrepo.com/show/475661/linkedin-color.svg"
+              loading="lazy"
+              alt="linkedin logo"
+            />
+            <span className="pt-0.5 text-sm pl-5">Continue with Linkedin</span>
+          </button>
         </div>
       </div>
     </>
