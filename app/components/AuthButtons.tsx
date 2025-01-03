42dabc3119fe3ebc805d5156ebc0b963d1a49549
Oguz Turkay <tyserract@gmail.com>
2025-01-03 18:05:02 +0300

    feat: enhance AuthButtons component with loading state management
    - Added loading state management to the AuthButtons component to improve user experience during OAuth processes.
    - Integrated appStore's updateAppData function to manage loading state when initiating Google and LinkedIn authentication.
    - Updated button text to reflect loading status, enhancing user feedback during authentication.
    - Ensured buttons are disabled while loading to prevent multiple submissions.

--- app/components/AuthButtons.tsx
+++ app/components/AuthButtons.tsx
@@ -1,20 +1,23 @@
-import { useGoogleLogin } from "@react-oauth/google";
-import { toast } from "react-toastify";
-import { userStore } from "~/store/user-store";
-import axios from "axios";
+import { useGoogleLogin } from '@react-oauth/google';
+import { toast } from 'react-toastify';
+import { userStore } from '~/store/user-store';
+import axios from 'axios';
+import { useState } from 'react'; // Import useState
+import { appStore } from '~/store/app-store';
 
 interface AuthButtonsProps {
   redirPath: string;
 }
 
 export function AuthButtons({ redirPath }: AuthButtonsProps) {
+  const { appData, updateAppData } = appStore();
+
   const redirectToLinkedIn = () => {
-    const clientId = "77bdm9yb1yeig5";
-    const redirectUri = encodeURIComponent(
-      `${window.location.origin}/${redirPath}`
-    );
+    updateAppData({ oauthLoading: true });
+    const clientId = '77bdm9yb1yeig5';
+    const redirectUri = encodeURIComponent(`${window.location.origin}/${redirPath}`);
     const state = Math.random().toString(36).substring(2);
-    const scope = "email openid profile ";
+    const scope = 'email openid profile ';
 
     const linkedInAuthUrl = `https://www.linkedin.com/oauth/v2/authorization?response_type=code&client_id=${clientId}&redirect_uri=${redirectUri}&state=${state}&scope=${scope}`;
 
@@ -23,8 +26,9 @@ export function AuthButtons({ redirPath }: AuthButtonsProps) {
 
   const login = useGoogleLogin({
     onSuccess: (tokenResponse) => {
+      updateAppData({ oauthLoading: true });
       axios
-        .get("https://www.googleapis.com/oauth2/v3/userinfo", {
+        .get('https://www.googleapis.com/oauth2/v3/userinfo', {
           params: {
             access_token: tokenResponse.access_token,
           },
@@ -34,14 +38,18 @@ export function AuthButtons({ redirPath }: AuthButtonsProps) {
             email: response.data.email,
             name: response.data.name,
           });
-          toast("Signed in successfully!", { type: "success" });
+          toast('Signed in successfully!', { type: 'success' });
+        })
+        .finally(() => {
+          updateAppData({ oauthLoading: false });
         });
     },
     onError: (error) => {
       console.log(error);
-      toast("An error occurred while signing in.", { type: "error" });
+      toast('An error occurred while signing in.', { type: 'error' });
+      updateAppData({ oauthLoading: false });
     },
-    flow: "implicit",
+    flow: 'implicit',
   });
 
   const { updateUserData } = userStore();
@@ -52,15 +60,16 @@ export function AuthButtons({ redirPath }: AuthButtonsProps) {
         <div className="w-64">
           <button
             onClick={() => login()}
-            className="w-64 px-2 py-2 border flex gap-2 border-slate-200 rounded text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
+            disabled={appData.oauthLoading} // Disable button if loading
+            className="w-64 px-2 py-2 border flex gap-2 border-slate-200 rounded text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150 disabled:opacity-50"
           >
             <img
               className="w-6 h-6"
               src="https://www.svgrepo.com/show/452216/google.svg"
               loading="lazy"
-              alt="linkedin logo"
+              alt="google logo"
             />
-            <span className="pt-0.5 text-sm w-full">Apply with Google</span>
+            <span className="pt-0.5 text-sm w-full">{appData.oauthLoading ? 'Loading...' : 'Apply with Google'}</span>
           </button>
         </div>
       </div>
@@ -69,7 +78,8 @@ export function AuthButtons({ redirPath }: AuthButtonsProps) {
         <div className="w-64">
           <button
             onClick={redirectToLinkedIn}
-            className="w-64 px-2 py-2 border flex gap-2 border-slate-200 rounded text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
+            disabled={appData.oauthLoading} // Disable button if loading
+            className="w-64 px-2 py-2 border flex gap-2 border-slate-200 rounded text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150 disabled:opacity-50"
           >
             <img
               className="w-6 h-6"
@@ -77,7 +87,7 @@ export function AuthButtons({ redirPath }: AuthButtonsProps) {
               loading="lazy"
               alt="linkedin logo"
             />
-            <span className="pt-0.5 text-sm w-full">Apply with Linkedin</span>
+            <span className="pt-0.5 text-sm w-full">{appData.oauthLoading ? 'Loading...' : 'Apply with LinkedIn'}</span>
           </button>
         </div>
       </div>
