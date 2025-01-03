41398c991df53fbbe5a4ba0f502105bc6a2031ed
Oguz Turkay <tyserract@gmail.com>
2025-01-03 12:57:58 +0300

    style: update LinkedInButton component for improved layout and accessibility
    - Adjusted button width for better alignment and usability.
    - Corrected alt text for the LinkedIn logo to enhance accessibility.

--- app/components/LinkedinButton.tsx
+++ app/components/LinkedinButton.tsx
@@ -19,13 +19,13 @@ export default function LinkedInLogin({ redirPath }: LinkedInLoginProps) {
   return (
     <button
       onClick={redirectToLinkedIn}
-      className="px-4 py-2 border flex gap-2 border-slate-200 rounded text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
+      className="w-64 px-2 py-2 border flex gap-2 border-slate-200 rounded text-slate-700 hover:border-slate-400 hover:text-slate-900 hover:shadow transition duration-150"
     >
       <img
         className="w-6 h-6"
         src="https://www.svgrepo.com/show/475661/linkedin-color.svg"
         loading="lazy"
-        alt="google logo"
+        alt="linkedin logo"
       />
       <span className="pt-0.5">Continue with Linkedin</span>
     </button>
